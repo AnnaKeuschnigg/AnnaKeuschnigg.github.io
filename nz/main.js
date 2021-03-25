43f2b3e7f2eac31a28f772a8cbb13448e27aa799
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 17:42:05 +0100

    {}

--- nz/main.js
+++ nz/main.js
@@ -17,17 +17,15 @@ const map= L.map("map",{//center:[stop.lat,stop.lng],
     ]}
 );
 
-console.log(ROUTE);
-for(let entry of ROUTE) {
-    console.log(entry);
+//console.log(ROUTE);
+for(let entry of ROUTE) {  //console.log(entry);
     let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
     mrk.bindPopup(`<h4>Stop ${stop.nr}: ${stop.name}<h4>
     <p><a href="${stop.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
     `);
     if (entry.nr==17) {
-        map.setView([entry.lat, entry.lng], 13);
-        mrk.openPopup();
-}
+        map.setView([entry.lat, entry.lng], 13),
+        mrk.openPopup();}}
 
 
-//console.log(document.querySelector("#map")
+//console.log(document.querySelector("#map"))
