f5c8ce18f1e23519d33b2d3d822c22814fc2d0c7
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 18:03:08 +0100

    stope?

--- nz/main.js
+++ nz/main.js
@@ -17,12 +17,19 @@ const map= L.map("map",{//center:[stop.lat,stop.lng],
     ]}
 );
 
-let nav= documents.querySelector("#navigation");
+let nav= document.querySelector("#navigation");
 console.log(nav);
 
 //console.log(ROUTE);
+ROUTE.sort((stop1, stop2)=>{
+    return stop1.nr> stop2.nr
+});
+
 for(let entry of ROUTE) {  //console.log(entry);
-    nav.innerHTML= `<option value= "${entry.user}">Stop${entry.nr}${entry.name}</option>`
+
+    nav.innerHTML += 
+    `<option value= "${entry.user}">Stop${entry.nr}${entry.name}</option>`;
+
     let mrk = L.marker([ entry.lat, entry.lng ]).addTo(map);
     mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
     <p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
