24abad1ad9cab97e805c0279eca7fdfcad81b0cc
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 17:02:42 +0100

    popup

--- nz/main.js
+++ nz/main.js
@@ -11,11 +11,13 @@ let stop = {
 
 
 const map= L.map("map",{center:[stop.lat,stop.lng],
-    zoom: 13,
+    zoom: 12,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]}
 );
 let mrk= L.marker([stop.lat,stop.lng]).addTo(map);
-mrk.bindPopup("Lake Rotoiti (Tasman)").openPopup();
+mrk.bindPopup(`<h4>${stop.nr}: ${stop.name}</h4>
+<p><i class="fas fa-external-link-alt mr-3"></i><a href="${stop.wikipedia}">Read about this stop in Wikipedia</a></p>
+`).openPopup();
 //console.log(document.querySelector("#map"))
