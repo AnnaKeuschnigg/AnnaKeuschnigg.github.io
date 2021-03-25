94c174b8078278eb823f9aa23f250ad63824db24
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 16:26:16 +0100

    tilelayer

--- nz/main.js
+++ nz/main.js
@@ -4,7 +4,7 @@ console.log(L);
 const map= L.map("map",{center:[-41.8245, 172.8381],
     zoom: 13,
     layers: [
-        L.titelLayer("https://{s}.titel.openstreetmap.org/{z}/{x}/{y}.png")
+        L.tileLayer("https://{s}.titel.openstreetmap.org/{z}/{x}/{y}.png")
     ]}
 );
 console.log(document.querySelector("#map"))
