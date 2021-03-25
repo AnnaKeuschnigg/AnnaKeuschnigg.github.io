37b9134a8b0ecc19015bdccaab111b51bd239a6f
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 18:20:54 +0100

    weiterverlinkung

--- nz/main.js
+++ nz/main.js
@@ -9,7 +9,6 @@ let stop = {
     wikipedia: "https://en.wikipedia.org/wiki/Lake_Rotoiti_(Tasman)"
 };
 
-
 const map= L.map("map",{//center:[stop.lat,stop.lng],
     //zoom: 12,
     layers: [
@@ -28,7 +27,7 @@ ROUTE.sort((stop1, stop2)=>{
 for(let entry of ROUTE) {  //console.log(entry);
 
     nav.innerHTML += 
-    `<option value= "${entry.user}">Stop${entry.nr}${entry.name}</option>`;
+    `<option value= "${entry.user}">Stop${entry.nr}: ${entry.name}</option>`;
 
     let mrk = L.marker([ entry.lat, entry.lng ]).addTo(map);
     mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
@@ -39,11 +38,13 @@ for(let entry of ROUTE) {  //console.log(entry);
         mrk.openPopup();}}
     
 nav.onchange= (evt) =>{
-    let selected= evt.target.selected.Index;
+    let selected= evt.target.selectedIndex;
     let options= evt.target.options;
     let value=options[selected].value;
     let link= `https://${value}.github.io/nz/index.html`;
     console.log(value, link);
+
+    window.location.href= link;
 };
 
 
