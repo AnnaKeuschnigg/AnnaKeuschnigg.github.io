3ef39ccda744a001f80be360e81eb2017e967889
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 16:25:26 +0100

    ,

--- nz/main.js
+++ nz/main.js
@@ -1,7 +1,7 @@
 console.log("Hello World");
 console.log(L);
 
-const map= L.map("map",{center:[-41.8245, 172.8381];
+const map= L.map("map",{center:[-41.8245, 172.8381],
     zoom: 13,
     layers: [
         L.titelLayer("https://{s}.titel.openstreetmap.org/{z}/{x}/{y}.png")
