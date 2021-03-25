bd2db91f981c87d72f0c78338288cc39dafb7409
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-03-25 16:43:29 +0100

    karte und marker=

--- nz/main.js
+++ nz/main.js
@@ -1,10 +1,21 @@
 console.log("Hello World");
 console.log(L);
+let stop = {
+    nr: 17,
+    name: "Lake Rotoiti",
+    lat: -41.8245,
+    lng:  172.8381,
+    user: "AnnaKeuschnigg",
+    Wikipedia: "https://en.wikipedia.org/wiki/Lake_Rotoiti_(Tasman)",
+};
 
-const map= L.map("map",{center:[-41.8245, 172.8381],
+
+const map= L.map("map",{center:[stop.lat,stop.lng],
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]}
 );
-console.log(document.querySelector("#map"))
+let mrk= L.marker([stop.lat,stop.lng]).addTo(map);
+mrk.bindPopup("Lake Rotoiti(Tasman)").openPopup();
+//console.log(document.querySelector("#map"))
