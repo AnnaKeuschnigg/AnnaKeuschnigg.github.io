51dd973895e67209933a30e77eee0fb96a9edb36
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-04-15 16:18:10 +0200

    irgendwas

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -7,3 +7,14 @@ let map= L.map("map", {
         basemapGray
     ]
 })
+
+let layerControl= L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto":L.tilelayer.provider('BasemapAT.orthofoto'),
+    "OpenRailwayMap":L.tilelayer.provider('OpenRailwayMap'),
+    "BasemapAT.overlay":L.tileLayer.provider('BasemapAT.overlay'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
+}).addTo(map);
