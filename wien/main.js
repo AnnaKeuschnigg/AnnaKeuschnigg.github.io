d47f07ee03ba5fe201e1a4fff5ee3a12fd59fc3e
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-04-29 16:37:42 +0200

    verbesserung

--- wien/main.js
+++ wien/main.js
@@ -61,35 +61,17 @@ let drawBusStop = (geojsonData) => {
                 })
             })
         }
-    }).addTo(map);
+    }).addTo(overlays.busStops);
 }
-//fetch("data/TOURISTIKHTSVSLOGD.json")
-//.then(response=>response.json())
-//.then(stations =>{
-//   L.geoJson(stations, {
-//        onEachFeature: (feature, layer) => {
-//   layer.bindPopup(feature.properties.STAT_NAME)
-//       },
-//  pointToLayer:(geoJsonPoint, latlng)=> {
-//          return L. marker(latlng, {
-//            icon: L.icon({
-//                  iconUrl: 'icons/busstop.png',
-//            iconSize: [38, 38]
-//      })
-//    })
-//        }
-//    }).addTo(map);
-//})
 
 for (let config of OGDWIEN) {
-    console.log("config:", config.data);
+    console.log("Config: ", config.data);
     fetch(config.data)
         .then(response => response.json())
         .then(geojsonData => {
-            console.log("Data:", geojsonData)
-            if (config.titel == "Haltestellen Vienna Sightseeing") {
-                drawBusStop();
+            console.log("Data: ", geojsonData);
+            if (config.title == "Haltestellen Vienna Sightseeing") {
+                drawBusStop(geojsonData);
             }
-          
         })
 }
