32a0da37f70658fafbf957ccdcbbc8bcdfc19fce
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-04-29 17:02:00 +0200

    ?

--- wien/main.js
+++ wien/main.js
@@ -61,7 +61,7 @@ let drawBusStop = (geojsonData) => {
                 })
             })
         },
-        
+
         attribution: '<a href= "https://data.wien.gv.at">Stadt Wien </a> <a href= "https://mapicons.mapsmarker.com">Maps Icons Collections</a>'
     }).addTo(overlays.busStops);
 }
