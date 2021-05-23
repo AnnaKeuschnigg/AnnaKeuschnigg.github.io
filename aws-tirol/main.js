8e508e72b7550a8cc181c45667f29303b0f6b1d9
AnnaKeuschnigg <a.keuschnigg@student.uibk.ac.at>
2021-05-23 12:39:42 +0200

    beautify

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -182,7 +182,7 @@ fetch(awsUrl)
         map.fitBounds(overlays.stations.getBounds());
     });
 
-        // minimap
+// minimap
 var miniMap = new L.Control.MiniMap(L.tileLayer.provider("BasemapAT.basemap"), {
     toggleDisplay: true,
     minimized: true
